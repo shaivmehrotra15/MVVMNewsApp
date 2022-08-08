4299e1f2d15b32a397f54d43832e33000fabaaca
shaiv mehrotra <shaivmehrotra15@gmail.com>
2022-08-08 22:29:10 +0530

    Second commit

--- app/src/main/java/com/androiddevs/mvvmnewsapp/app/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/app/ui/NewsActivity.kt
@@ -2,8 +2,11 @@ package com.androiddevs.mvvmnewsapp.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.app.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.app.repository.NewsRepository
 //import androidx.navigation.ui.setupWithNavController
 //import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.databinding.ActivityNewsBinding
@@ -17,6 +20,7 @@ class NewsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNewsBinding
     private lateinit var binding2: ItemArticlePreviewBinding
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,9 @@ class NewsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this , viewModelProviderFactory).get(NewsViewModel::class.java)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
 
 
--- app/src/main/java/com/androiddevs/mvvmnewsapp/app/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/app/ui/fragments/ArticleFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.app.ui.NewsActivity
+import com.androiddevs.mvvmnewsapp.app.ui.NewsViewModel
 import com.androiddevs.mvvmnewsapp.databinding.FragmentArticleBinding
 
 class ArticleFragment: Fragment(R.layout.fragment_article) {
 
     private lateinit var binding: FragmentArticleBinding
+    lateinit var viewModel: NewsViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
 
         binding = FragmentArticleBinding.bind(view)
     }
